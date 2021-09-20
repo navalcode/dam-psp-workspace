4d93b45fcfe2532a0c3ba6fc9a91c5cae3723789
Manuel Naval <navalrod@gmail.com>
2021-09-20 11:43:06 +0200

    nuevos cambios

---
+++ prueba/src/main/java/com/ejemplo1/prueba/Controlador.java
@@ -0,0 +1,16 @@
+package com.ejemplo1.prueba;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Controlador {
+
+    @GetMapping({"/", "/index"})
+    public String welcome() {
+        return
+
+
+    }
+
+}
