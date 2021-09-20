647902a21953323b70cd079f21851271b57361c5
Manuel Naval <navalrod@gmail.com>
2021-09-20 16:59:43 +0200

    repaso inicial y puesta a punto para recordar y recuperar algo de fluidez.

---
+++ demo/src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+@org.springframework.stereotype.Controller
+public class Controller {
+
+    @GetMapping("/")
+    public String verControllador (){
+        return "index";
+    }
+}
